722bf853cffedfd951f6b27ba600b8edb09457c0
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2023-11-08 23:50:35 +0100

    Add experience data

--- lib/data.ts
+++ lib/data.ts
@@ -24,3 +24,51 @@ export const links = [
     hash: "#contact",
   },
 ] as const;
+
+export const experience = [
+  {
+    title: "Full Stack Developer",
+    company: "FutureLearn (UK)",
+    dates: "May '23 - Current",
+    description: [
+      "Upgraded the entire stack of the old application, alongside refactoring, testing, and performance enhancements.",
+      "Currently assisting the frontend team in enhancing code quality and UX/UI.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company: "Aratech (Madrid)",
+    dates: "May '23 - Current",
+    description: [
+      "Improved code quality, addressed technical debt, and fixed performance issues.",
+      "Currently working on implementing new features for both the frontend and backend of software designed for a leading construction company.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company: "Top Div Solutions (Barcelona)",
+    dates: "January '21 - May '23 (2 years, 5 months)",
+    description: [
+      "Spearheaded the introduction of testing practices within the company.",
+      "Designed and developed a full-stack app for a fitness company, aiming for 100% coverage and optimal web performance.",
+    ],
+  },
+  {
+    title: "Interior Designer and Web Developer",
+    company: "Miquel Angel Muxi (Barcelona)",
+    dates: "January '17 - December '20 (4 years)",
+    description: [
+      "Conducted a complete redesign of the company website.",
+      "Enhanced the overall UX/UI quality and web performance, including responsiveness, which was previously lacking.",
+    ],
+  },
+  {
+    title: "Interior Designer, Graphic Designer and Web Developer",
+    company: "Cersis Grup International (Barcelona)",
+    dates: "October '14 - November '16 (2 years, 1 month)",
+    description: [
+      "Designed and developed small websites to facilitate project presentations.",
+      "Promoted to lead designer after a year and a half, with responsibilities including supervising colleagues’ designs and presentations quality.",
+    ],
+  },
+];
